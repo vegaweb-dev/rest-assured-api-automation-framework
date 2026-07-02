baa8171f225b51f484b3b1ceea123d374bc6b7f1
cvega <carlosjuniorrvega@hotmail.com>
2026-07-02 17:58:24 -0500

    test: add and run initial smoke test

---
+++ src/test/java/smoke/SmokeTest.java
@@ -0,0 +1,11 @@
+package smoke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SmokeTest {
+    @Test
+    void shouldRunFramework() {
+        assertTrue(true);
+    }
+}
